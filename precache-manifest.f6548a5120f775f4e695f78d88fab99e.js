d99affe2a9cd7b19f026c85d11e32143737d832b
Hung Le <leehxt@gmail.com>
2020-11-03 22:19:40 +0700

    Updates

---
+++ precache-manifest.f6548a5120f775f4e695f78d88fab99e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfde75244872d0b093f2174a584673a",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "96591bfcbdc974c2d36d",
+    "url": "/covid-tracker/static/css/main.16d43659.chunk.css"
+  },
+  {
+    "revision": "c0ed790609bc208f4c1d",
+    "url": "/covid-tracker/static/js/2.b17163d8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.b17163d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96591bfcbdc974c2d36d",
+    "url": "/covid-tracker/static/js/main.71bfa3e4.chunk.js"
+  },
+  {
+    "revision": "ef7fedef9cd7db49b0f7",
+    "url": "/covid-tracker/static/js/runtime-main.adeb3781.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
